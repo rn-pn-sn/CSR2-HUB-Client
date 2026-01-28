8f38877a2dc6ca3350ff5653f68eb3548bb6d46e
ABS <gmgm4434@gmail.com>
2026-01-28 18:58:53 +0000

    130up

--- release/ServiceWorker.js
+++ release/ServiceWorker.js
@@ -1,4 +1,4 @@
-﻿const BUILD_TIMESTAMP = "1769624960556";
+﻿const BUILD_TIMESTAMP = "1769626693082";
 const effectiveTimestamp = BUILD_TIMESTAMP || String(Date.now());
 const cacheName = "ABS-CSR2-HUB-Web-" + effectiveTimestamp;
 
